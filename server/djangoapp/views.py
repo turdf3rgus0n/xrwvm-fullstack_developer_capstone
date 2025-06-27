ebf25047fdf600149159522cf7b89feb27589390
turdf3rgus0n <rhk7898@yahoo.com>
2025-06-27 00:46:39 -0400

    Added changes for Module 4: Dynamic Pages

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -123,7 +123,7 @@ def get_dealer_details(request, dealer_id):
     if(dealer_id):
         endpoint = "/fetchDealer/"+str(dealer_id)
         dealership = get_request(endpoint)
-        return JsonResponse({"status":200,"dealer":dealership})
+        return JsonResponse({"status":200,"dealer": dealership[0]})
     else:
         return JsonResponse({"status":400,"message":"Bad Request"})
 
