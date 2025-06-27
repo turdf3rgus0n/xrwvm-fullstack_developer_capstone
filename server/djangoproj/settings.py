5496dafa5f2a8f1b7907eced946bffc1bdcbe188
turdf3rgus0n <rhk7898@yahoo.com>
2025-06-26 23:04:00 -0400

    Added changes for Module 3: Part 2, Django Models

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,11 +30,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://rhkmak-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'https://rhkmak-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://rhkmak-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'https://rhkmak-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
